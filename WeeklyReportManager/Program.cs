e82690edec139076e4e9e632ade7255a70cfaaa8
zBrunooDev <zbrunolima513@gmail.com>
2026-07-25 22:05:22 -0300

    feat: adicionando o menu e iniciando o método de ActivityLog

--- WeeklyReportManager/Program.cs
+++ WeeklyReportManager/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,26 @@ namespace WeeklyReportManager
     {
         static void Main(string[] args)
         {
+            List<ActivityReport> reports = new List<ActivityReport>();
+
+            Menu();
+
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("=== Relatório de atidade díario ===");
+            Console.WriteLine("Cadastre suas atividade com as informações abaixo: ");
+
+            Console.WriteLine("Caso queira encerrar digite o nome do da atividade 'Fim do Expediente'");
+        }
+
+        static void ActivityLog(string name, int quantity, string observation)
+        {
+            while (true)
+            {
+                
+            }
         }
     }
 }
